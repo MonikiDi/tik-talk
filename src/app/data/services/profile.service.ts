815614f9cb034a9f33d2f9af009a35d3ea350eb3
MonikiDi <d1e9n9i2s@gmail.com>
2024-10-26 20:22:10 +0300

    add interceptor

--- src/app/data/services/profile.service.ts
+++ src/app/data/services/profile.service.ts
@@ -13,4 +13,8 @@ export class ProfileService {
   getTestAccoounts() {
     return this.http.get<Profile[]>(`${this.baseApiUrl}account/test_accounts`);
   }
+
+  getMe() {
+    return this.http.get<Profile>(`${this.baseApiUrl}account/me`);
+  }
 }
