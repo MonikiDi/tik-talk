b4336ec60ed4fcff1cccec104da25ec8d946b8aa
MonikiDi <d1e9n9i2s@gmail.com>
2024-10-27 14:22:56 +0300

    rendered menu items using an ngFor loop

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
 import { SvgIconComponent } from '../svg-icon/svg-icon.component';
+import { NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ProfileCardComponent } from '../profile-card/profile-card.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [SvgIconComponent],
+  imports: [SvgIconComponent, NgFor, RouterLink, ProfileCardComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  menuItems = [
+    {
+      label: 'Моя страница',
+      icon: 'home',
+      link: '',
+    },
+    {
+      label: 'Чаты',
+      icon: 'chats',
+      link: 'chats',
+    },
+    {
+      label: 'Поиск',
+      icon: 'search',
+      link: 'search',
+    },
+  ];
+}
