7cd9f76057169be9ec3d4e33348b3073a264d16c
MonikiDi <d1e9n9i2s@gmail.com>
2024-12-03 11:26:24 +0300

    add post interface

---
+++ src/app/data/interfaces/post.interface.ts
@@ -0,0 +1,31 @@
+import {Profile} from './profile.interface';
+
+export interface PostCreateDto {
+  title: string;
+  content: string;
+  authorId: number
+  communityId: number
+}
+
+export interface Post {
+  id: number,
+  title: string,
+  communityId: number,
+  content: string,
+  author: Profile,
+  images: string[],
+  createdAt: string,
+  updatedAt: string,
+  likes: number,
+  comments: []
+}
+
+export interface Comment {
+  id: number,
+  text: string,
+  author: Profile,
+  postId: number,
+  commentId: number,
+  createdAt: string,
+  updatedAt: string,
+}
