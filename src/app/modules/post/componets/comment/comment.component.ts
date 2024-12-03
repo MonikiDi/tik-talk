a52f0adc2e4486a3188999a02470deaa4d75d156
MonikiDi <d1e9n9i2s@gmail.com>
2024-12-03 20:48:01 +0300

    add post comment verstka

--- src/app/modules/post/componets/comment/comment.component.ts
+++ src/app/modules/post/componets/comment/comment.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
+import {Comment} from '../../../../data/interfaces/post.interface';
+import {AvatarCircleComponent} from '../../../../common-ui/avatar-circle/avatar-circle.component';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-comment',
   standalone: true,
-  imports: [],
+  imports: [
+    AvatarCircleComponent,
+    DatePipe
+  ],
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.scss'
 })
 export class CommentComponent {
-
+comment= input<Comment>()
 }
--- src/app/modules/post/componets/post/post.component.ts
+++ src/app/modules/post/componets/post/post.component.ts
@@ -4,6 +4,7 @@ import {AvatarCircleComponent} from '../../../../common-ui/avatar-circle/avatar-
 import {DatePipe} from '@angular/common';
 import {SvgIconComponent} from '../../../../common-ui/svg-icon/svg-icon.component';
 import {PostInputComponent} from '../post-input/post-input.component';
+import {CommentComponent} from '../comment/comment.component';
 
 @Component({
   selector: 'app-post',
@@ -12,7 +13,8 @@ import {PostInputComponent} from '../post-input/post-input.component';
     AvatarCircleComponent,
     DatePipe,
     SvgIconComponent,
-    PostInputComponent
+    PostInputComponent,
+    CommentComponent
   ],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'
