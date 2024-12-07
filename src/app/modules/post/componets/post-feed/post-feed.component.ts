150fdb8f540d3efe202f409cb90acdbc3dc44b56
MonikiDi <d1e9n9i2s@gmail.com>
2024-12-07 23:47:44 +0300

    add decorator debounce

--- src/app/modules/post/componets/post-feed/post-feed.component.ts
+++ src/app/modules/post/componets/post-feed/post-feed.component.ts
@@ -1,8 +1,10 @@
 import {Component, ElementRef, HostListener, inject, Renderer2} from '@angular/core';
+import {PostService} from '../../ service/post.service';
+import {debounce, debounceTime, firstValueFrom, Subject} from "rxjs";
 import {PostInputComponent} from '../post-input/post-input.component';
 import {PostComponent} from '../post/post.component';
-import {PostService} from '../../ service/post.service';
-import {firstValueFrom} from "rxjs";
+import {Debounce} from '../../../../shared/decorators/debounce.decorator';
+
 
 @Component({
   selector: 'app-post-feed',
@@ -14,17 +16,13 @@ import {firstValueFrom} from "rxjs";
   templateUrl: './post-feed.component.html',
   styleUrl: './post-feed.component.scss'
 })
+
+
 export class PostFeedComponent {
-  postService = inject(PostService);
-  hostElement = inject(ElementRef);
-  r2 = inject(Renderer2);
-
+  public postService = inject(PostService);
+  public hostElement = inject(ElementRef);
+  public r2 = inject(Renderer2);
   feed = this.postService.posts
-
-  @HostListener('window: resize')
-  onWindowResize() {
-    this.resizeFeed()
-  }
 
   constructor() {
     firstValueFrom(this.postService.fetchPosts())
@@ -34,9 +32,55 @@ export class PostFeedComponent {
     this.resizeFeed()
   }
 
-  resizeFeed() {
+
+  // 1 Метод c debounceTime
+  // private readonly resize = new Subject<void>()
+  //
+  // @HostListener('window: resize')
+  // onWindowResize() {
+  //   this.resize.next(undefined)
+  // }
+  //
+  // public ngOnInit() {
+  //   this.resize.pipe(debounceTime(20)).subscribe(() => {
+  //     this.resizeFeed()
+  //   })
+  // }
+
+  // 2 Метод c Декоратором
+  @Debounce(20)
+  @HostListener('window: resize')
+  onWindowResize() {
+    this.resizeFeed()
+  }
+
+  // 3 Метод с функцией debounce
+  // @HostListener('window: resize')
+  // onWindowResize() {
+  //   this.func()
+  // }
+  // public func = this.debounce(this.resizeFeed.bind(this), 20)
+  // public debounce<Params extends any[]>(
+  //   func: (...args: Params) => any,
+  //   timeout: number,
+  // ): (...args: Params) => void {
+  //   let timer: any
+  //   return (...args: Params) => {
+  //     clearTimeout(timer)
+  //     timer = setTimeout(() => {
+  //       func(...args)
+  //     }, timeout)
+  //   }
+  // }
+
+  public resizeFeed() {
     const {top} = this.hostElement.nativeElement.getBoundingClientRect();
     const height = window.innerHeight - top - 48;
     this.r2.setStyle(this.hostElement.nativeElement, 'height', `${height}px`)
+    console.log(height);
   }
+
+
 }
+
+
---
+++ src/app/shared/decorators/debounce.decorator.ts
@@ -0,0 +1,12 @@
+export function Debounce(timeout: number) {
+  return (target: any, key: string, descriptor: PropertyDescriptor) => {
+    const method = descriptor.value
+    let timer: any
+    descriptor.value = function (...args: any[]) {
+      clearTimeout(timer)
+      timer = setTimeout(() => {
+        method.apply(this, args)
+      }, timeout)
+    }
+  };
+}
