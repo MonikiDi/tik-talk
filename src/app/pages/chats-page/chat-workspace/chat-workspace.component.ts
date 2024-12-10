f22c9d9177463211a312c6658b4fe996c2490017
MonikiDi <d1e9n9i2s@gmail.com>
2024-12-10 18:29:40 +0300

    added routing to components

--- src/app/pages/chats-page/chat-workspace/chat-workspace.component.ts
+++ src/app/pages/chats-page/chat-workspace/chat-workspace.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {ChatWorkspaceHeaderComponent} from './chat-workspace-header/chat-workspace-header.component';
+import {
+  ChatWorkspaceMessagesWrapperComponent
+} from './chat-workspace-messages-wrapper/chat-workspace-messages-wrapper.component';
+import {MessageInputComponent} from '../../../common-ui/message-input/message-input.component';
 
 @Component({
   selector: 'app-chat-workspace',
   standalone: true,
-  imports: [],
+  imports: [
+    ChatWorkspaceHeaderComponent,
+    ChatWorkspaceMessagesWrapperComponent,
+    MessageInputComponent
+  ],
   templateUrl: './chat-workspace.component.html',
   styleUrl: './chat-workspace.component.scss'
 })
