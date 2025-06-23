4467aff4654fdd00ab2497585fe7030e981521a8
MonikiDi <d1e9n9i2s@gmail.com>
2025-06-23 23:50:38 +0300

    feat. created actions lastMessageChatMap

--- libs/data-access/src/lib/chats/store/actions.ts
+++ libs/data-access/src/lib/chats/store/actions.ts
@@ -1,10 +1,12 @@
 import { createActionGroup, emptyProps, props } from '@ngrx/store';
-import { LastMessageRes } from '@tt/interfaces/chats';
+import { Chat, LastMessageRes } from '@tt/interfaces/chats';
 
 export const chatsActions = createActionGroup({
   source: 'chats',
   events: {
-    'load chats': emptyProps(),
-    'loaded chats': props<{ chats: LastMessageRes[] }>(),
+    'load getChatById': props<{ chatId: number }>(),
+    'loaded getChatById': props<{ chats: Chat }>(),
+    'load lastMessageChatMap': emptyProps(),
+    'loaded lastMessageChatMap': props<{ chats: LastMessageRes[] }>(),
   },
 });
