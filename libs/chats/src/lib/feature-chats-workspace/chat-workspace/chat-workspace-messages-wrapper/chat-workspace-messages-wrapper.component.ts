eae18c51670dfa33f419f650f270706b626b01d3
MonikiDi <d1e9n9i2s@gmail.com>
2025-06-24 16:14:23 +0300

    fix. removed request to get chats by id

--- libs/chats/src/lib/feature-chats-workspace/chat-workspace/chat-workspace-messages-wrapper/chat-workspace-messages-wrapper.component.ts
+++ libs/chats/src/lib/feature-chats-workspace/chat-workspace/chat-workspace-messages-wrapper/chat-workspace-messages-wrapper.component.ts
@@ -8,7 +8,6 @@ import {
   HostListener,
   inject,
   input,
-  OnInit,
   Renderer2,
   signal,
 } from '@angular/core';
@@ -22,12 +21,8 @@ import {
   normalizationText,
 } from '@tt/shared';
 import { Chat } from '@tt/interfaces/chats/chats.interface';
-import { firstValueFrom, switchMap, timer } from 'rxjs';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { DatePipe } from '@angular/common';
 import { ChatsService } from '@tt/data-access';
-
-const TIMEOUT = 5000;
 
 @Component({
   selector: 'app-chat-workspace-messages-wrapper',
@@ -41,10 +36,7 @@ const TIMEOUT = 5000;
   templateUrl: './chat-workspace-messages-wrapper.component.html',
   styleUrl: './chat-workspace-messages-wrapper.component.scss',
 })
-export class ChatWorkspaceMessagesWrapperComponent
-  implements OnInit, AfterViewInit
-{
-  destroyRef = inject(DestroyRef);
+export class ChatWorkspaceMessagesWrapperComponent implements AfterViewInit {
   chatsService = inject(ChatsService);
   r2 = inject(Renderer2);
   hostElement = inject(ElementRef);
@@ -70,17 +62,6 @@ export class ChatWorkspaceMessagesWrapperComponent
         top: this.hostElement.nativeElement.scrollHeight,
       });
     }
-  }
-
-  ngOnInit() {
-    timer(0, TIMEOUT)
-      .pipe(
-        switchMap(() => {
-          return this.chatsService.getChatById(this.chat().id);
-        }),
-        takeUntilDestroyed(this.destroyRef)
-      )
-      .subscribe();
   }
 
   ngAfterViewInit() {
@@ -113,7 +94,7 @@ export class ChatWorkspaceMessagesWrapperComponent
     // await firstValueFrom(
     //   this.chatsService.sendMessage(this.chat().id, messageText)
     // );
-    await firstValueFrom(this.chatsService.getChatById(this.chat().id));
+    // await firstValueFrom(this.chatsService.getChatById(this.chat().id));
     this.parentData.set('');
   }
 }
