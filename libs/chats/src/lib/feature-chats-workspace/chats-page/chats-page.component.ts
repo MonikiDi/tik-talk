8499d3979b5723b00b83c62db25c67ea8bf79cbe
MonikiDi <d1e9n9i2s@gmail.com>
2025-06-24 16:16:14 +0300

    refactoring

--- libs/chats/src/lib/feature-chats-workspace/chats-page/chats-page.component.ts
+++ libs/chats/src/lib/feature-chats-workspace/chats-page/chats-page.component.ts
@@ -1,15 +1,8 @@
-import {
-  Component,
-  DestroyRef,
-  inject,
-  OnDestroy,
-  OnInit,
-} from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChatsListComponent } from '../chats-list/chats-list.component';
 import { ChatsService } from '@tt/data-access';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-chats-page',
@@ -19,12 +12,12 @@ import { Subject } from 'rxjs';
   styleUrl: './chats-page.component.scss',
 })
 export class ChatsPageComponent implements OnInit {
-  #chatService = inject(ChatsService);
+  private readonly chatService = inject(ChatsService);
   private readonly destroyRef = inject(DestroyRef);
 
   ngOnInit() {
     // this.#chatService.connectWs();
-    this.#chatService
+    this.chatService
       .connectWs()
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe(console.log);
