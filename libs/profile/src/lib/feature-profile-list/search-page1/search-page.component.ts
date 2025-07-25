8d2567a0cac743a8f5969638ca2e327b550fd4ba
Denis Grigorev <d1e9n9i2s@gmail.com>
2025-07-25 15:35:22 +0300

    feat. start infinite scroll

--- libs/profile/src/lib/feature-profile-list/search-page1/search-page.component.ts
+++ libs/profile/src/lib/feature-profile-list/search-page1/search-page.component.ts
@@ -1,4 +1,12 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit, Signal, viewChild } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  OnInit,
+  Signal,
+  viewChild,
+} from '@angular/core';
 import { ProfileFiltersComponent } from '../profile-filters/profile-filters.component';
 import { PaginationPageComponent } from '@tt/common-ui';
 import { ProfileCardComponent } from '../../ui/profile-card/profile-card.component';
@@ -10,7 +18,7 @@ import {
   selectFilteredProfiles,
   selectLoadingProfiles,
   selectPagination,
-  selectProfiles
+  selectProfiles,
 } from '@tt/data-access';
 
 @Component({
@@ -19,24 +27,29 @@ import {
   imports: [
     ProfileFiltersComponent,
     PaginationPageComponent,
-    ProfileCardComponent
+    ProfileCardComponent,
   ],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SearchPageComponent implements OnInit {
   private readonly store = inject(Store);
-
   public readonly isPending: Signal<boolean> = this.store.selectSignal(
     selectLoadingProfiles
   );
-  public readonly items = toSignal(this.store.select(selectProfiles), {
-    initialValue: []
-  });
-
   public formFilter = viewChild.required(ProfileFiltersComponent);
   public pagination = this.store.selectSignal(selectPagination);
+  public currentPage = computed(() => {
+    return this.pagination().currentPage;
+  });
+  public perPage = 5;
+  public readonly items = toSignal(this.store.select(selectProfiles), {
+    initialValue: [],
+  });
+  public totalPages = computed(() => {
+    return this.pagination().totalPages;
+  });
 
   public ngOnInit() {
     this.formFilter()
@@ -47,8 +60,26 @@ export class SearchPageComponent implements OnInit {
     this.store.dispatch(
       profileActions.loadProfiles({
         filters: this.store.selectSignal(selectFilteredProfiles)(),
-        pagination: { currentPage: 1, perPage: 100 }
+        pagination: { currentPage: 1, perPage: 5 },
       })
     );
+  }
+
+  public onChangeCurrentPate(page: number) {
+    this.store.dispatch(
+      profileActions.paginationProfiles({ currentPage: page })
+    );
+  }
+
+  public onGoTo(page: number): void {
+    this.onChangeCurrentPate(page);
+  }
+
+  public onNext(page: number): void {
+    this.onChangeCurrentPate(page + 1);
+  }
+
+  public onPrevious(page: number): void {
+    this.onChangeCurrentPate(page - 1);
   }
 }
